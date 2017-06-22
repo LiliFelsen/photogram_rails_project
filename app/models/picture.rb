a9af1ac9fed53e56271b2438a9e7d80b5ff2248e
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-06-22 16:57:30 -0400

    styling in progress

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,7 +5,7 @@ class Picture < ApplicationRecord
   has_many :tags, through: :picture_tags
   accepts_nested_attributes_for :tags
 
-  has_attached_file :image, styles: { :large => "600x600>", :medium => "300x300>", :thumb => "100x100#" }
+  has_attached_file :image, styles: { :large => "600x600#", :medium => "300x300#", :thumb => "100x100#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
