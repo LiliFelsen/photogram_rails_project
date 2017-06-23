3d8efbf55d7d7037bb62bd1530cf6064b32a215a
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-06-23 09:22:04 -0400

    basic styling done, following done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'relationships/unfollow_user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'users#new'
+root 'sessions#new'
 
 get '/signup', to: 'users#new', as: 'signup'
 resources :users do
